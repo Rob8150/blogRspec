77836a3c64b1331068bfbd8f9977560e02296772
Robert Clark <robert.clark.1967@gmail.com>
2016-06-08 12:17:03 +0930

    Integrated Factory Girl

--- spec/features/add_posts_spec.rb
+++ spec/features/add_posts_spec.rb
@@ -4,17 +4,15 @@ RSpec.feature "adding posts" do
 	
 	scenario " allow a user to add a post" do
 	
-	 visit new_post_path
-	
-	 fill_in "Title", with: "My Title"
+	#post = create(:post)
+	#we can overite the factory value with our own value
+	post = create(:post, title: "My other Title")
 
-	 fill_in "Body", with: "My Body"
+	visit post_path(post)
 
-	 click_on('Create Post')
+	 expect(page).to have_content("My other Title")
 
-	 expect(page).to have_content("My Title")
-
-	 expect(page).to have_content("My Body")
+	 expect(page).to have_content("My New Body")
 	 
 	end
 
